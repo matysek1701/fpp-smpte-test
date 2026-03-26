5bafac1d52d6d212870e41555b1541e7fa0a458e
matysek1701 <matko.camaj@gmail.com>
2026-03-26 20:32:30 +0100

    NameChange forgotten reference

--- plugin_setup.php
+++ plugin_setup.php
@@ -1,5 +1,5 @@
 <div id="global" class="settings">
 <?
-PrintSettingGroup("SMPTETimeCodeInput", "", "", 1, "fpp-smpte");
+PrintSettingGroup("SMPTETimeCodeInput", "", "", 1, "fpp-smpte-test");
 ?>
 </div>
